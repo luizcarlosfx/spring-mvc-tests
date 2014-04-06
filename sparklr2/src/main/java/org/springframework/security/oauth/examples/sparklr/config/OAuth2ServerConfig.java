33fbf6546fa601b8f97b4d115ac2b41fe022f3b0
luizcarlosfx <luizcarlos.sfx@outlook.com>
2014-04-06 16:48:08 -0300

    First commit sparklr2

---
+++ sparklr2/src/main/java/org/springframework/security/oauth/examples/sparklr/config/OAuth2ServerConfig.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.security.oauth.examples.sparklr.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.OAuth2RequestFactory;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.token.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+/**
+ * @author Rob Winch
+ * 
+ */
+@Configuration
+public class OAuth2ServerConfig {
+
+	private static final String SPARKLR_RESOURCE_ID = "sparklr";
+
+	@Configuration
+	@Order(10)
+	protected static class UiResourceConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.requestMatchers().antMatchers("/photos/**")
+			.and()
+				.authorizeRequests()
+				.antMatchers("/photos").access("hasRole('ROLE_CLIENT')")
+				.antMatchers("/photos/trusted/**").access("hasRole('ROLE_CLIENT')")
+				.antMatchers("/photos/user/**").access("hasRole('ROLE_CLIENT')")
+				.antMatchers("/photos/**").access("hasRole('ROLE_CLIENT')");
+			// @formatter:on
+		}
+	}
+
+	@Configuration
+	@EnableResourceServer
+	protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+		@Override
+		public void configure(OAuth2ResourceServerConfigurer resources) {
+			resources.resourceId(SPARKLR_RESOURCE_ID);
+		}
+
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.requestMatchers().antMatchers("/photos/**", "/oauth/users/**", "/oauth/clients/**")
+			.and()
+				.authorizeRequests()
+					.antMatchers("/photos").access("#oauth2.hasScope('read')")
+					.antMatchers("/userinfo").access("#oauth2.hasScope('trust')")
+					.antMatchers("/photos/trusted/**").access("#oauth2.hasScope('trust')")
+					.antMatchers("/photos/user/**").access("#oauth2.hasScope('trust')")
+					.antMatchers("/photos/**").access("#oauth2.hasScope('read')")
+					.regexMatchers(HttpMethod.DELETE, "/oauth/users/([^/].*?)/tokens/.*")
+						.access("#oauth2.clientHasRole('ROLE_CLIENT') and (hasRole('ROLE_USER') or #oauth2.isClient()) and #oauth2.hasScope('write')")
+					.regexMatchers(HttpMethod.GET, "/oauth/users/.*")
+						.access("#oauth2.clientHasRole('ROLE_CLIENT') and (hasRole('ROLE_USER') or #oauth2.isClient()) and #oauth2.hasScope('read')")
+					.regexMatchers(HttpMethod.GET, "/oauth/clients/.*")
+						.access("#oauth2.clientHasRole('ROLE_CLIENT') and #oauth2.isClient() and #oauth2.hasScope('read')");
+			// @formatter:on
+		}
+
+	}
+
+	@Configuration
+	@EnableAuthorizationServer
+	protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+		private TokenStore tokenStore = new InMemoryTokenStore();
+
+		@Autowired
+		private OAuth2RequestFactory requestFactory;
+
+		@Autowired
+		@Qualifier("authenticationManagerBean")
+		private AuthenticationManager authenticationManager;
+
+		@Autowired
+		private ClientDetailsService clientDetailsService;
+
+		@Value("${tonr.redirect:http://localhost:8080/tonr2/sparklr/redirect}")
+		private String tonrRedirectUri;
+
+		@Override
+		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+			// @formatter:off
+			clients.inMemory().withClient("tonr")
+			 			.resourceIds(SPARKLR_RESOURCE_ID)
+			 			.authorizedGrantTypes("authorization_code", "implicit")
+			 			.authorities("ROLE_CLIENT")
+			 			.scopes("read", "write")
+			 			.secret("secret")
+			 		.and()
+			 		.withClient("tonr-with-redirect")
+			 			.resourceIds(SPARKLR_RESOURCE_ID)
+			 			.authorizedGrantTypes("authorization_code", "implicit")
+			 			.authorities("ROLE_CLIENT")
+			 			.scopes("read", "write")
+			 			.secret("secret")
+			 			.redirectUris(tonrRedirectUri)
+			 		.and()
+		 		    .withClient("my-client-with-registered-redirect")
+	 			        .resourceIds(SPARKLR_RESOURCE_ID)
+	 			        .authorizedGrantTypes("authorization_code", "client_credentials")
+	 			        .authorities("ROLE_CLIENT")
+	 			        .scopes("read", "trust")
+	 			        .redirectUris("http://anywhere?key=value")
+		 		    .and()
+	 		        .withClient("my-trusted-client")
+ 			            .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+ 			            .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+ 			            .scopes("read", "write", "trust")
+ 			            .accessTokenValiditySeconds(60)
+		 		    .and()
+	 		        .withClient("my-trusted-client-with-secret")
+ 			            .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+ 			            .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+ 			            .scopes("read", "write", "trust")
+ 			            .secret("somesecret")
+	 		        .and()
+ 		            .withClient("my-less-trusted-client")
+			            .authorizedGrantTypes("authorization_code", "implicit")
+			            .authorities("ROLE_CLIENT")
+			            .scopes("read", "write", "trust")
+     		        .and()
+		            .withClient("my-less-trusted-autoapprove-client")
+		                .authorizedGrantTypes("implicit")
+		                .authorities("ROLE_CLIENT")
+		                .scopes("read", "write", "trust")
+		                .autoApprove(true)
+		             .and()
+		             .withClient("unity")
+		            	.secret("secret")
+			            .authorizedGrantTypes("client_credentials")
+			            .authorities("ROLE_CLIENT")
+			            .scopes("read", "write", "trust");
+			// @formatter:on
+		}
+
+
+		@Bean
+		public ApprovalStore approvalStore() throws Exception {
+			TokenApprovalStore store = new TokenApprovalStore();
+			store.setTokenStore(tokenStore);
+			return store;
+		}
+
+		@Override
+		public void configure(OAuth2AuthorizationServerConfigurer oauthServer) throws Exception {
+			oauthServer.tokenStore(tokenStore).authenticationManager(authenticationManager).realm("sparklr2/client");
+		}
+
+	}
+
+}
